e0e4c092a20640cbb4edffb83d29520215c00c67
safiworks <116169309+safiworks@users.noreply.github.com>
2025-08-21 06:38:21 +0300

    feat(libopal): wait_for_events_blocking and handle events

--- libopal/src/lib.rs
+++ libopal/src/lib.rs
@@ -6,11 +6,13 @@ use std::{
 use opal_abi::com::{
     packet::MAX_PACKET_SIZE,
     request::{Request, RequestKind},
-    response::{OkResponse, Response},
+    response::{OkResponse, Response, event::Event},
 };
 use safa_api::sockets::UnixSockConnection;
 
 pub mod window;
+
+static EVENTS_QUEUE: Mutex<Vec<Event>> = Mutex::new(Vec::new());
 
 static WM_CONNECTION: LazyLock<Mutex<UnixSockConnection>> = LazyLock::new(|| {
     use safa_api::sockets::{SockKind, UnixSockConnectionBuilder};
@@ -28,17 +30,57 @@ static WM_CONNECTION: LazyLock<Mutex<UnixSockConnection>> = LazyLock::new(|| {
 pub(crate) fn send_request(req: RequestKind) -> io::Result<Response> {
     let request = Request::new(req);
     let (bytes, len) = request.encode();
+    let mut events = EVENTS_QUEUE
+        .lock()
+        .expect("Failed to acquire lock on events queue");
     let mut wm = WM_CONNECTION.lock().expect("Failed to lock WM connection");
 
     Write::write(&mut *wm, &bytes[..len])?;
 
+    let mut packet: [u8; MAX_PACKET_SIZE] = [0u8; MAX_PACKET_SIZE];
+    let response = loop {
+        let read = Read::read(&mut *wm, &mut packet)?;
+
+        let msg = &packet[..read];
+
+        let response = Response::decode(msg).expect("Couldn't Parse WM's response");
+        match response {
+            Response::Event(event) => {
+                events.push(event);
+            }
+            other => break other,
+        }
+    };
+    Ok(response)
+}
+
+/// Blockingly wait for an event from the window manager.
+pub fn wait_for_event_blocking() -> io::Result<Event> {
+    {
+        let mut events = EVENTS_QUEUE
+            .lock()
+            .expect("Failed to acquire lock on events queue");
+
+        if let Some(event) = events.pop() {
+            return Ok(event);
+        }
+    }
+
+    let mut wm = WM_CONNECTION.lock().expect("Failed to lock WM connection");
     let mut packet: [u8; MAX_PACKET_SIZE] = [0u8; MAX_PACKET_SIZE];
     let read = Read::read(&mut *wm, &mut packet)?;
 
     let msg = &packet[..read];
 
     let response = Response::decode(msg).expect("Couldn't Parse WM's response");
-    Ok(response)
+    match response {
+        Response::Event(event) => {
+            return Ok(event);
+        }
+        other => unreachable!(
+            "Shouldn't get any other kind of responses while waiting for events, got: {other:#?}"
+        ),
+    }
 }
 
 /// Initializes the client that is going to communicate with the WM
